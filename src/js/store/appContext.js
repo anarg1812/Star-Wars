7b8ca9c8c819897080ae7bb23ea62e8736bfb6f4
Ana Romero <anamaria_20romero@hotmail.com>
2024-04-27 10:10:30 +0000

    home

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -4,11 +4,10 @@ import getState from "./flux.js";
 // Don't change, here is where we initialize our context, by default it's just going to be null.
 export const Context = React.createContext(null);
 
-// This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
-// https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
+
 const injectContext = PassedComponent => {
 	const StoreWrapper = props => {
-		//this will be passed as the contenxt value
+		//this will be passed as the context value
 		const [state, setState] = useState(
 			getState({
 				getStore: () => state.store,
@@ -31,11 +30,11 @@ const injectContext = PassedComponent => {
 			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
 			 *
 			 **/
+			state.actions.getPeopleData();
+			state.actions.getPlanetsData();
+			state.actions.getStarShipsData();
 		}, []);
 
-		// The initial value for the context is not null anymore, but the current state of this component,
-		// the context will now have a getStore, getActions and setStore functions available, because they were declared
-		// on the state of this component
 		return (
 			<Context.Provider value={state}>
 				<PassedComponent {...props} />
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,58 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import CharCard from "../component/charCard.js";
+import PlanetCard from "../component/planetCard.js";
+import ShipCard from "../component/shipCard.js";
+import { Context } from "../store/appContext.js";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store } = useContext(Context);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2 className="heading">Characters</h2>
+        </Col>
+      </Row>
+      <Row className="mb-5">
+        <div className="d-flex justify-content-between overFlow">
+          {store.people
+            ? store.people.map((elem, index) => (
+                <CharCard key={index} id={++index} character={elem} />
+              ))
+            : ""}
+        </div>
+      </Row>
+      <Row>
+        <Col>
+          <h2 className="heading">Planets</h2>
+        </Col>
+      </Row>
+      <Row Row className="mb-5">
+        <div className="d-flex justify-content-between overFlow">
+          {store.planets
+            ? store.planets.map((elem, index) => (
+                <PlanetCard key={index} id={++index} planet={elem} />
+              ))
+            : ""}
+        </div>
+      </Row>
+      <Row>
+        <Col>
+          <h2 className="heading">Start Ships</h2>
+        </Col>
+      </Row>
+      <Row Row className="mb-5">
+        <div className="d-flex justify-content-between overFlow">
+          {store.starShips
+            ? store.starShips.map((elem, index) => (
+                <ShipCard key={index} id={++index} ship={elem} />
+              ))
+            : ""}
+        </div>
+      </Row>
+    </Container>
+  );
+};
